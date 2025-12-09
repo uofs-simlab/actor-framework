e0a77f10371543585416df70e9d4eeb6a4af26f7
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-12-09 17:12:56 -0600

    Refactored to avoid circular dependencies, added pragma once and have a return all behaviors method now.

--- libcaf_cuda/caf/cuda/control-layer/behavior_table.hpp
+++ libcaf_cuda/caf/cuda/control-layer/behavior_table.hpp
@@ -1,33 +1,33 @@
+#pragma once
 #include "caf/cuda/control-layer/token.hpp"
 #include "caf/cuda/control-layer/behavior.hpp"
 #include "caf/cuda/control-layer/behavior_token.hpp"
 #include <unordered_map>
 #include <string>
 
+
 namespace caf::cuda {
+
+class scheduler_actor_behavior;
+class behavior_token;
 
 class behavior_table {
 public:
-    behavior_table() {
-        // TODO insert behaviors here
+    behavior_table() = default;
+
+    void add(const std::string& name, scheduler_actor_behavior* beh) {
+        table_[name] = beh;
     }
 
-    /*
-     * Given a behavior_token, return a scheduler_actor_behavior*.
-     * Returns nullptr if nothing is found.
-     */
-    scheduler_actor_behavior* getBehavior(behavior_token tok) {
-
-        auto it = table.find(tok.getBehavior());
-
-        if (it != table.end())
-            return it->second;
-
-        return nullptr;
+    scheduler_actor_behavior* get(const behavior_token& tok) const {
+        auto it = table_.find(tok.name());
+        return it != table_.end() ? it->second : nullptr;
     }
+
+    auto& all_behaviors() { return table_; }
 
 private:
-    std::unordered_map<std::string, scheduler_actor_behavior*> table;
+    std::unordered_map<std::string, scheduler_actor_behavior*> table_;
 };
 
 } // namespace caf::cuda
