6d665c25f1f12b2c5820adea0d0bb9e5eeee8661
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-12-09 17:34:56 -0600

    Added unsafe message declarations.

--- libcaf_cuda/caf/cuda/control-layer/all-control-layer.hpp
+++ libcaf_cuda/caf/cuda/control-layer/all-control-layer.hpp
@@ -4,5 +4,7 @@
 #include "caf/cuda/control-layer/launch_response_token.hpp"
 
 
+CAF_ALLOW_UNSAFE_MESSAGE_TYPE(caf::cuda::token_ptr)
+//TODO may need to add launch and launch response token pointer
 
 
