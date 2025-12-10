f8e3f112e3d3f76af7f3a339c787ecd0613d4d24
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-12-10 17:39:57 -0600

    Revert "Moved implementation out of the headers and into the cpp files to reduce software bloat and chances of circular dependencies."
    This reverts commit f525777b11aefe715b4ffd8254815d4874e3facf.

--- libcaf_cuda/caf/cuda/control-layer/green_light_behavior.hpp
+++ libcaf_cuda/caf/cuda/control-layer/green_light_behavior.hpp
@@ -6,12 +6,24 @@ namespace caf::cuda {
 
 class green_light_behavior : public scheduler_actor_behavior {
 public:
-    void schedule() override;
-    void receive(scheduler_actor_state* state, const token_ptr& tok) override;
+    void schedule() override {
+        // scheduling logic, if needed
+    }
+
+    void receive(scheduler_actor_state* state, const token_ptr& tok) override {
+        // flush the queue
+        while (!state->queue.empty()) {
+            auto queued = state->queue.front();
+            state->queue.pop();
+            // respond to queued token (demo: just print)
+        }
+
+        // handle current token immediately
+        // (demo: just print)
+    }
 };
 
-// Declare externally defined instance
-extern green_light_behavior GREEN_BEHAVIOR;
+inline green_light_behavior GREEN_BEHAVIOR;
 
 } // namespace caf::cuda
 
--- libcaf_cuda/caf/cuda/control-layer/red_light_behavior.hpp
+++ libcaf_cuda/caf/cuda/control-layer/red_light_behavior.hpp
@@ -5,12 +5,16 @@ namespace caf::cuda {
 
 class red_light_behavior : public scheduler_actor_behavior {
 public:
-    void schedule() override;
-    void receive(scheduler_actor_state* state, const token_ptr& tok) override;
+    void schedule() override {
+        // scheduling logic, if needed
+    }
+
+    void receive(scheduler_actor_state* state, const token_ptr& tok) override {
+        state->queue.push(tok); // enqueue everything
+    }
 };
 
-// Declare externally defined instance
-extern red_light_behavior RED_BEHAVIOR;
+inline red_light_behavior RED_BEHAVIOR;
 
 } // namespace caf::cuda
 
