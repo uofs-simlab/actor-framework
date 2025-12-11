bf565585efaf360f491b8c8242cbb4ee2ff92737
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-12-11 15:49:13 -0600

    Moved implementation logic out of the hpp file. Change was made to ensure code is cleaner and does not cause future errors.

--- libcaf_cuda/caf/cuda/control-layer/green_light_behavior.hpp
+++ libcaf_cuda/caf/cuda/control-layer/green_light_behavior.hpp
@@ -1,29 +1,15 @@
 #pragma once
 #include "caf/cuda/control-layer/behavior.hpp"
-#include "caf/cuda/control-layer/launch_token.hpp"
 
 namespace caf::cuda {
 
 class green_light_behavior : public scheduler_actor_behavior {
 public:
-    void schedule() override {
-        // scheduling logic, if needed
-    }
+    void schedule() override;
 
-    void receive(scheduler_actor_state* state, const token_ptr& tok) override {
-        // flush the queue
-        while (!state->queue.empty()) {
-            auto queued = state->queue.front();
-            state->queue.pop();
-            // respond to queued token (demo: just print)
-        }
-
-        // handle current token immediately
-        // (demo: just print)
-    }
+    void receive(scheduler_actor_state* state, const token_ptr& tok) override; 
 };
 
-inline green_light_behavior GREEN_BEHAVIOR;
 
 } // namespace caf::cuda
 
---
+++ libcaf_cuda/src/control-layer/green_light_behavior.cpp
@@ -0,0 +1,24 @@
+#include "caf/cuda/control-layer/green_light_behavior.hpp"
+
+namespace caf::cuda {
+
+    void green_light_behavior::schedule() {
+    //TODO IMPLEMENT
+    }
+
+    void green_light_behavior::receive(scheduler_actor_state* state, const token_ptr& tok)    {
+        // flush the queue
+        while (!state->queue.empty()) {
+            auto queued = state->queue.front();
+            state->queue.pop();
+            // respond to queued token (demo: just print)
+        }
+
+        // handle current token immediately
+        // (demo: just print)
+    }
+};
+
+
+} // namespace caf::cuda
+
