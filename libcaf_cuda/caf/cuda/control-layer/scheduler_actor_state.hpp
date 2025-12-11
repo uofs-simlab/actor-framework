9a7f92ef45e90044ba90879f373cacc8ddcc66ec
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-12-11 16:12:12 -0600

    ADded a self_handle attribute.

--- libcaf_cuda/caf/cuda/control-layer/scheduler_actor_state.hpp
+++ libcaf_cuda/caf/cuda/control-layer/scheduler_actor_state.hpp
@@ -8,6 +8,7 @@ namespace caf::cuda {
 class scheduler_actor_behavior; 
 
 struct scheduler_actor_state {
+    caf::actor  self_handle;
     scheduler_actor_behavior* current_behavior = nullptr;
     behavior_table table;
     std::queue<token_ptr> queue;
