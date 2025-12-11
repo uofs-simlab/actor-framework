1dc5434cbe5e2c6eb39feef3e060685b55447cf5
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-12-11 15:11:48 -0600

    Fixed syntax error with schedule method.

--- libcaf_cuda/caf/cuda/control-layer/red_light_behavior.hpp
+++ libcaf_cuda/caf/cuda/control-layer/red_light_behavior.hpp
@@ -5,9 +5,7 @@ namespace caf::cuda {
 
 class red_light_behavior : public scheduler_actor_behavior {
 public:
-    void schedule() override {
-        // scheduling logic, if needed
-    }
+    void schedule() override;
 
     void receive(scheduler_actor_state* state, const token_ptr& tok) override;
 
