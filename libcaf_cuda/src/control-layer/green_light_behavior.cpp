2fae812e28081a72bcff526572cee077ee5b7135
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-12-11 16:38:18 -0600

    Implemented init method.

--- libcaf_cuda/src/control-layer/green_light_behavior.cpp
+++ libcaf_cuda/src/control-layer/green_light_behavior.cpp
@@ -1,4 +1,6 @@
+#include "caf/cuda/control-layer/all-control-layer.hpp"
 #include "caf/cuda/control-layer/green_light_behavior.hpp"
+
 
 namespace caf::cuda {
 
@@ -17,6 +19,18 @@ namespace caf::cuda {
         // handle current token immediately
         // (demo: just print)
     }
+
+   void green_light_behavior::init(scheduler_actor_state * state) {
+	   std::cout << "GREEN LIGHT\n";
+	   behavior_token_ptr red_light = make_behavior_token("red");
+
+	   //send a request to change behavior to green light after 5 seconds
+    	   anon_mail(red_light)
+		   .delay(std::chrono::seconds(5))
+		   .send(state -> self);	
+    }
+
+
 
 } // namespace caf::cuda
 
