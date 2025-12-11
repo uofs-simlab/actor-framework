4b25b657464d03a7bb07f709536b987c1224be36
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-12-11 15:44:25 -0600

    Commented out program ptr instrusive pointer logic. CHange was made since for some reason program pointer acting as instrusive pointer causes segfaults with inappropriate deleting, will need to fix this later but it works for now.

--- libcaf_cuda/caf/cuda/program.hpp
+++ libcaf_cuda/caf/cuda/program.hpp
@@ -27,11 +27,16 @@ public:
   CUfunction get_kernel(int device_id);
 
     friend void intrusive_ptr_add_ref(const program* p) noexcept {
-        p->ref_count_.fetch_add(1, std::memory_order_relaxed);
+        //p->ref_count_.fetch_add(1, std::memory_order_relaxed);
     }
     friend void intrusive_ptr_release(const program* p) noexcept {
-        if (p->ref_count_.fetch_sub(1, std::memory_order_acq_rel) == 1)
-            delete p;
+        if (p->ref_count_.fetch_sub(1, std::memory_order_acq_rel) == 1) {
+	   //WARNING TURNING THIS ON FOR SOME REASON, CAUSES SEGFAUTLS
+	   //I HAVE NO IDEA WHY 
+	   //TODO FIX THIS 
+	   // std::cout<< "Deleting\n";
+           // delete p;
+	}
     }
 
 
