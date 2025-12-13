9ad15733968c6c840de39d2529d2013a3ca297d6
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-12-13 12:45:05 -0600

    Moved allocation over to heap allocation.

--- libcaf_cuda/src/control-layer/token_factory.cpp
+++ libcaf_cuda/src/control-layer/token_factory.cpp
@@ -5,28 +5,31 @@
 
 namespace caf::cuda {
 
+// Use manual heap allocation with intrusive_ptr
 token_ptr make_launch_token(program_ptr prog,
                             nd_range range,
                             int memory_usage,
                             std::string id,
                             actor reply_to)
 {
-    return caf::make_counted<launch_token>(std::move(prog),
-                                           std::move(range),
-                                           memory_usage,
-                                           std::move(id),
-                                           reply_to);
+    return token_ptr(
+        new launch_token(std::move(prog),
+                         std::move(range),
+                         memory_usage,
+                         std::move(id),
+                         reply_to));
 }
 
 token_ptr make_launch_response_token(actor receiver,
                                      const launch_token& orig)
 {
-    return caf::make_counted<launch_response_token>(receiver, orig);
+    return token_ptr(new launch_response_token(receiver, orig));
 }
 
 behavior_token_ptr make_behavior_token(std::string name)
 {
-    return caf::make_counted<behavior_token>(std::move(name));
+    return behavior_token_ptr(new behavior_token(std::move(name)));
 }
 
 } // namespace caf::cuda
+
