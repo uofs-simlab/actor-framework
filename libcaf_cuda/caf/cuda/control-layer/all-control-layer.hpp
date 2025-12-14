3a8fe05375db8ec28139711d53ef89b2fc544f98
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2025-12-14 10:53:15 -0600

    Changed registered message types to adhere to cafs message passing policy.

--- libcaf_cuda/caf/cuda/control-layer/all-control-layer.hpp
+++ libcaf_cuda/caf/cuda/control-layer/all-control-layer.hpp
@@ -1,31 +1,41 @@
 #pragma once
+
 #include "caf/cuda/all.hpp"
+
+// Control-layer object types (ref-counted, non-copyable)
 #include "caf/cuda/control-layer/token.hpp"
 #include "caf/cuda/control-layer/launch_token.hpp"
 #include "caf/cuda/control-layer/launch_response_token.hpp"
 #include "caf/cuda/control-layer/behavior_token.hpp"
+
+// Control-layer logic
 #include "caf/cuda/control-layer/behavior.hpp"
 #include "caf/cuda/control-layer/scheduler_actor.hpp"
 #include "caf/cuda/control-layer/token_factory.hpp"
 
+// -----------------------------------------------------------------------------
+// Type ID block
+//   Register ONLY the types that may appear by value in caf::message.
+//   Ref-counted control objects are passed exclusively via intrusive_ptr.
+// -----------------------------------------------------------------------------
+
 CAF_BEGIN_TYPE_ID_BLOCK(cuda_control, caf::first_custom_type_id + 200)
-// You can also use  caf::id_block::core::end  instead of the +200 if you prefer
 
-  CAF_ADD_TYPE_ID(cuda_control, (caf::cuda::token))
-CAF_ADD_TYPE_ID(cuda_control, (caf::intrusive_ptr<caf::cuda::token>))  
-//CAF_ADD_TYPE_ID(cuda_control, (caf::cuda::behavior_token))
-  CAF_ADD_TYPE_ID(cuda_control, (caf::intrusive_ptr<caf::cuda::behavior_token>)) // ← and this one too if you ever use it
-	//CAF_ADD_TYPE_ID(cuda_control, (caf::cuda::launch_token))
-  //CAF_ADD_TYPE_ID(cuda_control, (caf::cuda::launch_response_token))
-
-  // Very commonly needed as well – add them now so you don’t hit the error later
+CAF_ADD_TYPE_ID(cuda_control, (caf::intrusive_ptr<caf::cuda::token>))
+CAF_ADD_TYPE_ID(cuda_control, (caf::intrusive_ptr<caf::cuda::behavior_token>))
+CAF_ADD_TYPE_ID(cuda_control, (caf::intrusive_ptr<caf::cuda::launch_token>))
+CAF_ADD_TYPE_ID(cuda_control, (caf::intrusive_ptr<caf::cuda::launch_response_token>))
 
 CAF_END_TYPE_ID_BLOCK(cuda_control)
 
-// Optional but harmless – keep your old macros (they silence the “unsafe” warning)
+// -----------------------------------------------------------------------------
+// Unsafe message types
+//   These are explicitly local-only, non-serializable control objects.
+//   CAF will reject any attempt to send them over the network.
+// -----------------------------------------------------------------------------
+
 CAF_ALLOW_UNSAFE_MESSAGE_TYPE(caf::cuda::token)
-CAF_ALLOW_UNSAFE_MESSAGE_TYPE(caf::cuda::token_ptr)
 CAF_ALLOW_UNSAFE_MESSAGE_TYPE(caf::cuda::behavior_token)
-CAF_ALLOW_UNSAFE_MESSAGE_TYPE(caf::cuda::behavior_token_ptr)
 CAF_ALLOW_UNSAFE_MESSAGE_TYPE(caf::cuda::launch_token)
 CAF_ALLOW_UNSAFE_MESSAGE_TYPE(caf::cuda::launch_response_token)
+
