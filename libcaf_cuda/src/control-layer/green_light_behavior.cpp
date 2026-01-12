53539e11d495b7cdb1fc73688069a9f9fbaf6f37
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2026-01-12 16:27:14 -0600

    Made changes to methods to reflect changes made in response tokens constructors and factory functions.

--- libcaf_cuda/src/control-layer/green_light_behavior.cpp
+++ libcaf_cuda/src/control-layer/green_light_behavior.cpp
@@ -11,10 +11,14 @@ void green_light_behavior::receive(scheduler_actor_state* state,
                                    const token_ptr& tok) {
 
     if (tok->getType() == LAUNCH) {
-        process_launch_token(tok, state->self);
+	//use 0 as stream id for now, eventually will have to figure out
+	//stream load balancing 
+        process_launch_token(tok, state->self,state -> device_number,0);
     }
     else if (tok->getType() == MEMORY) {
-        process_memory_transfer_token(tok, state->self);
+	//use 0 as stream id for now, eventually will have to figure out
+	//stream load balancing 
+        process_memory_transfer_token(tok, state->self,state -> device_number,0);
     }
 
     //this may cause an issue if a message is never received then 
@@ -52,13 +56,16 @@ void green_light_behavior::receive(scheduler_actor_state* state,
 //--------------------------------------------------
 // process_launch_token
 //--------------------------------------------------
-void green_light_behavior::process_launch_token(const token_ptr& tok,caf::actor self) {
+void green_light_behavior::process_launch_token(const token_ptr& tok,
+		caf::actor self,
+		int device_number,
+		int stream_id) {
     caf::cuda::launch_token& launch =
         static_cast<caf::cuda::launch_token&>(*tok);
 
     // Create response token using the existing launch token
     caf::cuda::token_ptr response =
-        make_launch_response_token(self, launch);
+        make_launch_response_token(self, launch,device_number,stream_id);
 
     // Send response to the actor that requested the launch
     anon_mail(response).send(launch.getReplyActor());
@@ -68,13 +75,15 @@ void green_light_behavior::process_launch_token(const token_ptr& tok,caf::actor 
 // process_memory_transfer_token
 //--------------------------------------------------
 void green_light_behavior::process_memory_transfer_token(const token_ptr& tok,
-                                                         caf::actor self) {
+                                                         caf::actor self,
+							 int device_number,
+							 int stream_id) {
     caf::cuda::memory_transfer_token& mem =
         static_cast<caf::cuda::memory_transfer_token&>(*tok);
 
     // Create memory response token
     caf::cuda::token_ptr response =
-        make_memory_response_token(self, mem);
+        make_memory_response_token(self, mem,device_number,stream_id);
 
     // Send response to requesting actor
     anon_mail(response).send(mem.getReplyActor());
