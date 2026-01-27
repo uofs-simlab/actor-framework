3231d131a315d8991913368cdafbab063302f127
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2026-01-27 14:46:47 -0600

    Added comments.

--- libcaf_cuda/caf/cuda/control-layer/request_token.hpp
+++ libcaf_cuda/caf/cuda/control-layer/request_token.hpp
@@ -1,9 +1,17 @@
 #pragma once
+/*
+ * Meant to be superclass of all tokens send to the scheduler actor
+ * unfortunately got cut mostly due to the fact that 
+ * it requires too much refactoring then has time to do
+ * Will hopefully come back to this class
+ */
+
+
 
 #include "caf/cuda/control-layer/token.hpp"
 
 
-#define INDEPENDENT -1
+//#define INDEPENDENT -1
 
 
 namespace caf::cuda {
