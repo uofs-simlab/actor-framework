6a61b36f75a52c815a920c17388e9ffc47552b2a
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2026-01-27 14:01:18 -0600

    Updated state to include std::vector of kernel graphs, so that scheduler actor can map operation dependencies correctly.

--- libcaf_cuda/caf/cuda/control-layer/scheduler_actor_state.hpp
+++ libcaf_cuda/caf/cuda/control-layer/scheduler_actor_state.hpp
@@ -1,8 +1,10 @@
 #pragma once
 #include <queue>
+#include <vector>
 #include "caf/cuda/control-layer/behavior_table.hpp"
-#include "caf/cuda/control-layer/behavior_token.hpp"  // Still needed for behavior_token
+#include "caf/cuda/control-layer/behavior_token.hpp" 
 #include "caf/cuda/control-layer/token.hpp"
+#include "caf/cuda/control-layer/kernel_graph.hpp"
 
 namespace caf::cuda {
 class scheduler_actor_behavior; 
@@ -11,7 +13,8 @@ struct scheduler_actor_state {
     caf::actor  self;
     scheduler_actor_behavior* current_behavior = nullptr;
     behavior_table table;
-    std::queue<token_ptr> queue;
+    std::queue<token_ptr> queue; // here for legacy prototype schedulers
+    std::vector<kernel_graph> operations; //more modern dependency based data structure 
     int device_number;
 };
 } // namespace caf::cuda
