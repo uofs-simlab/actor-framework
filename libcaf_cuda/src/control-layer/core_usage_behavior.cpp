2a65586a0d76aac3e19902a8af30552918852fe9
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2026-01-28 17:14:59 -0600

    Implemented init_state method.

--- libcaf_cuda/src/control-layer/core_usage_behavior.cpp
+++ libcaf_cuda/src/control-layer/core_usage_behavior.cpp
@@ -1,5 +1,7 @@
 #include "caf/cuda/control-layer/all-control-layer.hpp"
 #include "caf/cuda/control-layer/core_usage_behavior.hpp"
+#include "caf/cuda/manager.hpp"
+#include "caf/cuda/device.hpp"
 
 namespace caf::cuda {
 
@@ -26,5 +28,15 @@ void core_usage_behavior::receive(const token_ptr& tok) {
     
 }
 
+
+void core_usage_behavior::init_state() {
+
+	dev = manager::get.find_device(state-> device_number);
+	heuristic = core_heuristic_function(dev);
+	total_SM =  dev -> num_sms();
+	available_SM = total_sm;
+	available_memory = static_cast<int>(dev -> total_memory_bytes());
+
+}
 
 } // namespace caf::cuda
