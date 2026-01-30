64851d31b14d6e847d909893b000c976eaf56c0b
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2026-01-30 14:35:34 -0600

    Implemented getCost(token_ptr) method since overloads with same name are hidden in c++ and thats not what I want here.

--- libcaf_cuda/caf/cuda/control-layer/core_usage_behavior.hpp
+++ libcaf_cuda/caf/cuda/control-layer/core_usage_behavior.hpp
@@ -38,13 +38,13 @@ private:
    //data structures to manage dependencies
    std::unordered_map<int,kernel_graph> graphs;
    std::vector<kernel_graph> independent_graphs;
-   std::vector<kernel_graph> best_graphs; //should contain top 5-10 best selections ideally or something along the lines
+   std::vector<kernel_graph*> best_graphs; //should contain top 5-10 best selections ideally or something along the lines
 
    void init_state();
    void create_new_graph(const token_ptr& token); //this should either add to indepedent or graphs data structure
 			    //note to self use std::move for cheap copies
    
-   void rank(); //this should rank the graphs (high to low) for best canidates
+   void rank(std::size_t); //this should rank the graphs (high to low) for best canidates
 
    int get_next_stream(); // this should return the next stream based on some decisions
    void dummy_schedule(); //just a test scheduler algorthim do not use seriously
