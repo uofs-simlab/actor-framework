91cb608804187e3b9fd34ae3f9c8ce5837785d0a
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2026-02-10 14:14:24 -0600

    Removed logging.

--- libcaf_cuda/src/control-layer/green_light_behavior.cpp
+++ libcaf_cuda/src/control-layer/green_light_behavior.cpp
@@ -8,7 +8,7 @@ green_light_behavior::green_light_behavior(scheduler_actor_state& state)
     : scheduler_actor_behavior(state) {}
 
 void green_light_behavior::on_enter() {
-    std::cout << "GREEN LIGHT\n";
+    //std::cout << "GREEN LIGHT\n";
 
     behavior_token_ptr red_light = make_behavior_token("red");
     //send a request to change behavior to red light after 5 seconds
