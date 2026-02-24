7e101e883741d167cdfd6bd2ffbf24d7e41f0c62
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2026-02-24 12:59:48 -0600

    Updated config to indicate usage for the memory manager actor.

--- libcaf_cuda/caf/cuda/manager_config.hpp
+++ libcaf_cuda/caf/cuda/manager_config.hpp
@@ -10,11 +10,14 @@ class manager_config {
 public:
     manager_config() : scheduler_on(false) {}      // initialize the bool
     manager_config(bool scheduler) : scheduler_on(scheduler) {}
-
-    bool getSchedulerOn() const { return scheduler_on; } // should be const
+    manager_config(bool scheduler, bool memory_manager) : scheduler_on(scheduler), memory_manager_on(memory_manager) {}
+    
+    bool getSchedulerOn() const { return scheduler_on; }
+    bool getMemoryManagerOn() const { return memory_manager_on; }
 
 private:
     bool scheduler_on;
+    bool memory_manager_on = false;
 };
 
 } // namespace caf::cuda
