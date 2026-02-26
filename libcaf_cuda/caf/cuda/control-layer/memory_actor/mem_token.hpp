d18281137b0886437f809c0756aad87d3a4776bc
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2026-02-26 15:41:02 -0600

    Renamed memory_response_token to mem_token since name was already taken.

--- libcaf_cuda/caf/cuda/control-layer/memory_actor/mem_token.hpp
+++ libcaf_cuda/caf/cuda/control-layer/memory_actor/mem_token.hpp
@@ -8,17 +8,17 @@ namespace caf::cuda {
 
 /// RAII-style memory tracker that returns memory to the memory actor
 /// when it goes out of scope.
-class CAF_CUDA_EXPORT memory_response_token {
+class CAF_CUDA_EXPORT mem_token {
 public:
-    mem_response_token(std::size_t memory_in_bytes,
-                          int device_number,
-                          caf::actor memory_actor_handle) 
+    mem_token(std::size_t memory_in_bytes,
+              int device_number,
+              caf::actor memory_actor_handle) 
         : memory_bytes_(memory_in_bytes),
           device_number_(device_number),
           memory_actor_(std::move(memory_actor_handle)) {}
 
     // Move constructor (allow moving, but disable copying)
-    memory_response_token(memory_response_token&& other) noexcept
+    mem_token(mem_token&& other) noexcept
         : memory_bytes_(other.memory_bytes_),
           device_number_(other.device_number_),
           memory_actor_(std::move(other.memory_actor_)) 
@@ -26,7 +26,7 @@ public:
         other.memory_bytes_ = 0;  // prevent double return
     }
 
-    memory_response_token& operator=(memory_response_token&& other) noexcept {
+    mem_token& operator=(mem_token&& other) noexcept {
         if (this != &other) {
             memory_bytes_ = other.memory_bytes_;
             device_number_ = other.device_number_;
@@ -37,15 +37,13 @@ public:
     }
 
     // Delete copy constructor/assignment to avoid double returns
-    memory_response_token(const memory_response_token&) = delete;
-    memory_response_token& operator=(const memory_response_token&) = delete;
+    mem_token(const mem_token&) = delete;
+    mem_token& operator=(const mem_token&) = delete;
 
-    ~memory_response_token() {
+    ~mem_token() {
         if (memory_bytes_ > 0 && memory_actor_) {
             // RAII return: tell memory actor we freed memory
-            anon_mail(ack(CAF_CUDA_ACK_MEMORY))
-                .arg(device_number_)
-                .arg(memory_bytes_)
+            anon_mail(device_number_,memory_bytes_)
                 .send(memory_actor_);
         }
     }
