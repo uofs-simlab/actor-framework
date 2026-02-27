beb9e08661110f98b3df7f50fb6c29c893a692ce
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2026-02-27 17:53:44 -0600

    Initial commit.

---
+++ libcaf_cuda/src/control-layer/exit_actor/exit_actor.cpp
@@ -0,0 +1,24 @@
+#include "caf/cuda/control-layer/all-control-layer.hpp"
+#include "caf/cuda/manager.hpp"
+
+namespace caf::cuda {
+caf::behavior exit_actor_fun(caf::stateful_actor<exit_actor_state>* self,int limit) {
+
+
+        return {
+                [=](int num_completed) {
+                        self->state().completed += num_completed;
+
+                        std::cout << "Actors finished is " << self->state().completed << "\n";
+                        if (self->state().completed >= limit) {
+
+                                caf::cuda::manager::shutdown();
+                                self->quit();
+                        }
+                }
+        };
+
+
+}
+} //namespace caf::cuda
+
