f52fdd0e45e9199e6ee8f418f49d6d2ab5619add
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2026-02-27 17:22:28 -0600

    Updated constructors to be caf compliant.

--- libcaf_cuda/caf/cuda/control-layer/memory_actor/mem_token.hpp
+++ libcaf_cuda/caf/cuda/control-layer/memory_actor/mem_token.hpp
@@ -10,35 +10,22 @@ namespace caf::cuda {
 /// when it goes out of scope.
 class CAF_CUDA_EXPORT mem_token {
 public:
-    mem_token(std::size_t memory_in_bytes,
+
+   //for caf id system
+    mem_token() = default;
+    
+	mem_token(std::size_t memory_in_bytes,
               int device_number,
               caf::actor memory_actor_handle) 
         : memory_bytes_(memory_in_bytes),
           device_number_(device_number),
           memory_actor_(std::move(memory_actor_handle)) {}
 
-    // Move constructor (allow moving, but disable copying)
-    mem_token(mem_token&& other) noexcept
-        : memory_bytes_(other.memory_bytes_),
-          device_number_(other.device_number_),
-          memory_actor_(std::move(other.memory_actor_)) 
-    {
-        other.memory_bytes_ = 0;  // prevent double return
-    }
 
-    mem_token& operator=(mem_token&& other) noexcept {
-        if (this != &other) {
-            memory_bytes_ = other.memory_bytes_;
-            device_number_ = other.device_number_;
-            memory_actor_ = std::move(other.memory_actor_);
-            other.memory_bytes_ = 0;  // prevent double return
-        }
-        return *this;
-    }
 
     // Delete copy constructor/assignment to avoid double returns
-    mem_token(const mem_token&) = delete;
-    mem_token& operator=(const mem_token&) = delete;
+    //mem_token(const mem_token&) = delete;
+    //mem_token& operator=(const mem_token&) = delete;
 
     ~mem_token() {
         if (memory_bytes_ > 0 && memory_actor_) {
