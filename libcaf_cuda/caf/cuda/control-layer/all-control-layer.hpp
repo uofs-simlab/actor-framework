3b54019d5190b8d361b0f54401371ca8c0e43478
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2026-02-27 17:18:09 -0600

    Updated to include mem_token.

--- libcaf_cuda/caf/cuda/control-layer/all-control-layer.hpp
+++ libcaf_cuda/caf/cuda/control-layer/all-control-layer.hpp
@@ -26,6 +26,7 @@
 //memory actor includes
 #include "caf/cuda/control-layer/memory_actor/memory_actor.hpp"
 #include "caf/cuda/control-layer/memory_actor/memory_request_token.hpp"
+#include "caf/cuda/control-layer/memory_actor/mem_token.hpp"
 
 
 // -----------------------------------------------------------------------------
@@ -42,6 +43,7 @@ CAF_ADD_TYPE_ID(cuda_control, (caf::intrusive_ptr<caf::cuda::response_token>))
 CAF_ADD_TYPE_ID(cuda_control, (caf::intrusive_ptr<caf::cuda::memory_transfer_token>))
 CAF_ADD_TYPE_ID(cuda_control, (caf::intrusive_ptr<caf::cuda::memory_response_token>))
 CAF_ADD_TYPE_ID(cuda_control, (caf::cuda::memory_request_token))
+CAF_ADD_TYPE_ID(cuda_control, (caf::cuda::mem_token))
 CAF_ADD_TYPE_ID(cuda_control, (caf::intrusive_ptr<caf::cuda::transfer_token>))
 CAF_ADD_TYPE_ID(cuda_control, (std::vector<caf::intrusive_ptr<caf::cuda::token>>))
 CAF_ADD_TYPE_ID(cuda_control, (caf::cuda::kernel_graph))
@@ -61,6 +63,7 @@ CAF_ALLOW_UNSAFE_MESSAGE_TYPE(caf::cuda::launch_token)
 CAF_ALLOW_UNSAFE_MESSAGE_TYPE(caf::cuda::launch_response_token)
 CAF_ALLOW_UNSAFE_MESSAGE_TYPE(caf::cuda::memory_transfer_token)
 CAF_ALLOW_UNSAFE_MESSAGE_TYPE(caf::cuda::memory_request_token)
+CAF_ALLOW_UNSAFE_MESSAGE_TYPE(caf::cuda::mem_token)
 CAF_ALLOW_UNSAFE_MESSAGE_TYPE(caf::cuda::memory_response_token)
 CAF_ALLOW_UNSAFE_MESSAGE_TYPE(caf::cuda::response_token)
 CAF_ALLOW_UNSAFE_MESSAGE_TYPE(caf::cuda::transfer_token)
