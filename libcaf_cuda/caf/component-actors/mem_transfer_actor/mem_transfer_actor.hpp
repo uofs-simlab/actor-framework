e9c1d48479c3b3052ea4bfbcc1e2d06497c6fe10
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2026-02-28 11:33:40 -0600

    Fixed compiler errors.

--- libcaf_cuda/caf/component-actors/mem_transfer_actor/mem_transfer_actor.hpp
+++ libcaf_cuda/caf/component-actors/mem_transfer_actor/mem_transfer_actor.hpp
@@ -14,13 +14,12 @@ namespace caf::cuda {
 
 template <class T>
 void mem_transfer_actor_fun(caf::blocking_actor* self) {
-  self->receive_while([&] { return true; })(
-    [&](mem_ptr<T> d_mem) {
 
-      // Blocking call happens safely here
-      std::vector<T> host_data = d_mem->copy_to_host();
+  self->receive(
+    [&](mem_ptr<T> d_mem) -> std::vector<T> {
 
-      self->reply(std::move(host_data));
+      // blocking call is safe here
+      return d_mem->copy_to_host();
     }
   );
 }
