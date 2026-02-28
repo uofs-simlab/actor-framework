5bae609b9d3164f0ca9dce77fcd7f13ffba6ff67
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2026-02-28 12:46:15 -0600

    Updated actor to fix issues reguarding length of the kernel being templated with type T when it should have been type int.

--- libcaf_cuda/caf/component-actors/vector_add_actor/vector_add_actor.hpp
+++ libcaf_cuda/caf/component-actors/vector_add_actor/vector_add_actor.hpp
@@ -1,4 +1,5 @@
 #pragma once
+
 #include <caf/all.hpp>
 #include "caf/cuda/all.hpp"
 
@@ -16,7 +17,7 @@ using vector_add_command = command_runner<
     mem_ptr<T>,  // input A
     mem_ptr<T>,  // input B
     out<T>,      // output C
-    in<T>        // length N
+    in<int>      // length N (always an int)
 >;
 
 // Actor behavior
@@ -26,40 +27,47 @@ caf::behavior vector_add_actor_fun(
     program_ptr vector_add_kernel)
 {
     using mem_t = mem_ptr<T>;
+    using runner_t = vector_add_command<T>;
 
-    vector_add_command<T> runner;  // non-static, per actor
     self->state().vector_add_kernel = vector_add_kernel;
+    runner_t runner;  // per-actor runner instance
 
     return {
         [=](mem_t vecA,
             mem_t vecB,
             int N,
             int device_number,
-            int stream_id)
-         mutable ->  mem_ptr<T>
+            int stream_id) mutable -> mem_t
         {
-            nd_range dims((N + 255) / 256, 1, 1, 256, 1, 1);
+            program_ptr kernel = self->state().vector_add_kernel;
 
-            auto arg3 = create_out_arg<T>(N);
-            auto arg4 = create_in_arg<T>(N);
+            const int THREADS = 256;
+            const int BLOCKS = (N + THREADS - 1) / THREADS;
 
+            nd_range dims(BLOCKS, 1, 1, THREADS, 1, 1);
+
+            out<T> arg_out  = create_out_arg<T>(N);
+            in<int> arg_len = create_in_arg<int>(N);  // length as int
+
+            // Run the kernel asynchronously
             auto result_tuple = runner.run_async(
-                vector_add_kernel,
+                kernel,
                 dims,
                 stream_id,
-                0,
-                device_number,
+                0,             // shared_memory
+                device_number, // device
                 vecA,
                 vecB,
-                arg3,
-                arg4
+                arg_out,
+                arg_len
             );
 
-            // Extract mem_ptr<T> result (2nd index of result tuple)
-            return std::get<2>(result_tuple);
+            // Extract the output buffer (3rd element of tuple)
+            mem_t output_device_buffer = std::get<2>(result_tuple);
+
+            return output_device_buffer;
         }
     };
 }
 
 } // namespace caf::cuda
-
