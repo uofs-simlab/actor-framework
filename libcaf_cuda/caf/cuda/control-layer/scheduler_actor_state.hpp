39c4caba67e9924f7bde2972343251502cd29435
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2026-03-12 11:04:32 -0600

    Changed num_streams to 500 to coinside with max number of streams possible.

--- libcaf_cuda/caf/cuda/control-layer/scheduler_actor_state.hpp
+++ libcaf_cuda/caf/cuda/control-layer/scheduler_actor_state.hpp
@@ -17,7 +17,7 @@ struct scheduler_actor_state {
     std::queue<token_ptr> queue; // here for legacy prototype schedulers
     std::vector<kernel_graph> operations; //more modern dependency based data structure 
     int device_number;
-    int num_streams = 150; // number of streams that can be used by the scheduler
+    int num_streams = 500; // number of streams that can be used by the scheduler
     std::vector<caf::actor> schedulers; //the other scheduler actors in the system, an actor for a 
 			     //specific GPU can be accessed via there corrosponding 
 			     //device number in the std::vector
