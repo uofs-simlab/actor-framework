4c85f96e1dcc427fe9b34be1e9ade14566522bd6
Kyle Klenk <kyle.c.klenk@gmail.com>
2026-03-25 11:59:56 -0600

    Removed unnecessary super-sub-class relationship

--- libcaf_cuda/caf/cuda/actor_facade.hpp
+++ libcaf_cuda/caf/cuda/actor_facade.hpp
@@ -4,16 +4,19 @@
 #include <caf/actor_cast.hpp>
 #include <caf/actor_config.hpp>
 #include <caf/actor_system.hpp>
+#include <caf/event_based_actor.hpp>
 
-#include "caf/cuda/actor_facade_event.hpp"
+#include "caf/cuda/command.hpp"
+#include "caf/cuda/helpers.hpp"
+#include "caf/cuda/platform.hpp"
+#include "caf/cuda/program.hpp"
+
 
 namespace caf::cuda {
 
 template <bool PassConfig, class... Ts>
-class actor_facade : public actor_facade_event<PassConfig, Ts...> {
+class actor_facade : public event_based_actor {
 public:
-  using super = actor_facade_event<PassConfig, Ts...>;
-
   static caf::actor create(caf::actor_system& sys,
                            caf::actor_config&&,
                            program_ptr program,
@@ -32,9 +35,72 @@ public:
       sys->spawn<actor_facade>(std::move(program), std::move(dims)));
   }
 
-  actor_facade(caf::actor_config& cfg, program_ptr prog, nd_range dims)
-    : super(cfg, std::move(prog), std::move(dims)) {
+  actor_facade(caf::actor_config& cfg, program_ptr program, nd_range dims)
+    : caf::event_based_actor(cfg),
+      program_(std::move(program)),
+      dims_(std::move(dims)) {
   }
+
+  ~actor_facade() override {
+    platform::create()->release_streams_for_actor(this->id());
+  }
+
+  caf::behavior make_behavior() override {
+    return {
+      [this](const caf::message& msg) -> caf::result<std::vector<output_buffer>> {
+        try {
+          if (auto outputs = handle_message(msg))
+            return std::move(*outputs);
+          return caf::make_error(caf::sec::unexpected_message,
+                                 "actor_facade received unsupported arguments");
+        } catch (const std::exception& ex) {
+          return caf::make_error(caf::sec::runtime_error, ex.what());
+        }
+      },
+    };
+  }
+
+private:
+  template <class... Us>
+  std::vector<output_buffer> launch_kernel(Us&&... xs) {
+    using command_t = command<caf::actor, std::decay_t<Us>...>;
+    auto cmd = caf::make_counted<command_t>(program_, dims_, actor_id_,
+                                            std::forward<Us>(xs)...);
+    return cmd->enqueue();
+  }
+  
+
+  template <std::size_t... Is>
+  std::vector<output_buffer>
+  launch_wrapped_message(const caf::message& msg, size_t offset,
+                         std::index_sequence<Is...>) {
+    return launch_kernel(msg.get_as<Ts>(Is + offset)...);
+  }
+
+  template <std::size_t... Is>
+  std::vector<output_buffer>
+  launch_raw_message(const caf::message& msg, size_t offset,
+                     std::index_sequence<Is...>) {
+    return launch_kernel(Ts{msg.get_as<raw_t<Ts>>(Is + offset)}...);
+  }
+
+
+  std::optional<std::vector<output_buffer>> handle_message(const caf::message& msg) {
+    auto indices = std::index_sequence_for<Ts...>{};
+    if (msg.match_elements<Ts...>())
+      return launch_wrapped_message(msg, 0, indices);
+    if (msg.match_elements<raw_t<Ts>...>())
+      return launch_raw_message(msg, 0, indices);
+    if (msg.match_elements<caf::actor, Ts...>())
+      return launch_wrapped_message(msg, 1, indices);
+    if (msg.match_elements<caf::actor, raw_t<Ts>...>())
+      return launch_raw_message(msg, 1, indices);
+    return std::nullopt;
+  }
+
+  program_ptr program_;
+  nd_range dims_;
+  int actor_id_;
 };
 
 } // namespace caf::cuda
--- libcaf_cuda/src/helpers.cpp
+++ libcaf_cuda/src/helpers.cpp
@@ -1,15 +1,6 @@
 #include "caf/cuda/helpers.hpp"
 
 namespace caf::cuda {
-//gets a random number
-CAF_CUDA_EXPORT int random_number() {
-    static std::random_device rd;
-    static std::mt19937 gen(rd());
-    static std::uniform_int_distribution<int> distrib(INT_MIN, INT_MAX);
-    return distrib(gen);  
-}
-
-
 //helper function designed to read in a cubin from file
 std::vector<char> load_cubin(const std::string& filename) {
   std::ifstream in(filename, std::ios::binary);
