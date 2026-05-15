e2b7d8d106f383e287bc4bb968049b3c6f397aa3
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2026-05-15 10:17:21 -0600

    fixed compiler errors.

--- libcaf_cuda/caf/cuda/actor_facade.hpp
+++ libcaf_cuda/caf/cuda/actor_facade.hpp
@@ -9,15 +9,14 @@
 #include <caf/actor_system.hpp>
 #include <caf/anon_mail.hpp>
 #include <caf/event_based_actor.hpp>
-#include <caf/response_promise.hpp>
 #include <cuda.h>
-#include <utility> // For std::index_sequence, std::integral_constant
 
 #include "caf/cuda/command.hpp"
 #include "caf/cuda/global.hpp"
 #include "caf/cuda/helpers.hpp"
 #include "caf/cuda/platform.hpp"
 #include "caf/cuda/program.hpp"
+#include "caf/cuda/command_runner.hpp"
 
 
 namespace caf::cuda {
@@ -34,22 +33,22 @@ public:
   static caf::actor create(caf::actor_system& sys,
                            program_ptr program,
                            nd_range dims,
-                           atom_value reply_atom = 0) {
+                           int reply_id = 0) {
     return caf::actor_cast<caf::actor>(
-      sys.spawn<actor_facade>(std::move(program), std::move(dims), reply_atom));
+      sys.spawn<actor_facade>(std::move(program), std::move(dims), reply_id));
   }
 
-  actor_facade(caf::actor_config& cfg, program_ptr program, nd_range dims, atom_value reply_atom)
+  actor_facade(caf::actor_config& cfg, program_ptr program, nd_range dims, int reply_id)
     : caf::event_based_actor(cfg),
       program_(std::move(program)),
       dims_(std::move(dims)),
-      reply_atom_(reply_atom) {
+      reply_id_(reply_id) {
     actor_id_ = this->id();
   }
 
   ~actor_facade() override {
-      command_runner<Ts...> runner;
-      runner.release_stream_for_actor(actor_id_);
+    command_runner<> runner;
+    runner.release_stream_for_actor(actor_id_);
   }
 
   caf::behavior make_behavior() override {
@@ -58,7 +57,6 @@ public:
         enqueue_impl(device_num, stream_id, std::move(output_indices), std::forward<Ts>(args)...);
       },
       [this](int device_num, int stream_id, Ts... args) {
-        // Copy everything back if indices are omitted
         enqueue_impl(device_num, stream_id, {}, std::forward<Ts>(args)...);
       },
       [this](int device_num, std::vector<int> output_indices, Ts... args) {
@@ -84,81 +82,65 @@ private:
   template <class... Us>
   void enqueue_impl(int device_num, int stream_id, std::vector<int> output_indices, Us&&... xs) {
     command_runner<Ts...> runner;
-    // Launch kernel asynchronously
     auto results = runner.run_async(program_, dims_, stream_id, 0, 
                                     device_num, std::forward<Us>(xs)...);
 
     auto sender = this->current_sender();
-    auto r_atom = reply_atom_;
+    auto r_id = reply_id_;
 
-    // If no indices specified, default to copying all arguments back
     if (output_indices.empty()) {
       for (int i = 0; i < static_cast<int>(sizeof...(Ts)); ++i) {
         output_indices.push_back(i);
       }
     }
 
-    // Track if we actually queued any transfers to host
-    // Even if we don't, we will send a final 'done' signal via callback
-    // to ensure the stream is drained.
+    for (int idx : output_indices) {
+      if (idx >= 0 && idx < static_cast<int>(sizeof...(Ts))) {
+        // Dispatch runtime index to compile-time sequence
+        dispatch_index(idx, [&](auto current_idx_constant) {
+          constexpr std::size_t Index = current_idx_constant;
+          using MemPtrType = std::tuple_element_t<Index, mem_tuple>;
+          using ValueType = typename MemPtrType::element_type::value_type;
 
-    if (!output_indices.empty()) {
-        // Helper to dispatch a lambda based on a runtime index
-        auto switch_on_index = [&](int runtime_idx, auto&& func, auto... Is) {
-            ([&] {
-                if (runtime_idx == Is) {
-                    func(std::integral_constant<int, Is>{});
-                }
-            }(), ...);
-        };
+          MemPtrType mem_ptr = std::get<Index>(results);
 
-        for (int idx : output_indices) {
-            if (idx >= 0 && idx < sizeof...(Ts)) {
-                switch_on_index(idx, [&](auto current_idx_constant) {
-                    // Compile-time index extraction
-                    constexpr std::size_t Index = current_idx_constant; // Compile-time index
-                    using MemPtrType = std::tuple_element_t<Index, mem_tuple>;
-                    using ValueType = typename MemPtrType::element_type::value_type;
-
-                    MemPtrType mem_ptr = std::get<Index>(results);
-
-                    // Only copy if the mem_ptr is valid and has OUT or IN_OUT access
-                    if (mem_ptr && (mem_ptr->access() == OUT || mem_ptr->access() == IN_OUT)) {
-                        runner.copy_to_host_async(mem_ptr, 
-                          [sender, r_atom, Index](std::vector<ValueType>&& data) {
-                            // Send back the data along with the original index
-                            if (sender) { // Ensure sender is still valid
-                                if (r_atom != 0) {
-                                    caf::anon_mail(r_atom, Index, std::move(data)).send(sender);
-                                } else {
-                                    caf::anon_mail(Index, std::move(data)).send(sender);
-                                }
-                            }
-                        });
-                    }
-                }, std::make_index_sequence<sizeof...(Ts)>{});
-            } else {
-                this->println("Warning: Output index {} is out of bounds (0-{})", idx, sizeof...(Ts) - 1);
-            }
-        }
+          if (mem_ptr && (mem_ptr->access() == OUT || mem_ptr->access() == IN_OUT)) {
+            runner.copy_to_host_async(mem_ptr, [sender, r_id, Index](std::vector<ValueType>&& data) {
+              if (sender) {
+                // Send: Correlation ID, Argument Index, Data Vector
+                caf::anon_mail(r_id, static_cast<int>(Index), std::move(data)).send(sender);
+              }
+            });
+          }
+        });
+      } else {
+        this->println("Warning: Output index {} is out of bounds", idx);
+      }
     }
 
-    // Finally, alert the sender that everything is finished.
-    // This callback is queued on the stream after all kernel and transfer commands.
-    runner.add_callback(stream_id, device_num, [sender, r_atom]() mutable {
+    // Final completion alert (Correlation ID, -1 to signal "all finished")
+    runner.add_callback(stream_id, device_num, [sender, r_id]() mutable {
       if (sender) {
-        if (r_atom != 0)
-          caf::anon_mail(r_atom, gpu_done_atom_v).send(sender);
-        else
-          caf::anon_mail(gpu_done_atom_v).send(sender);
+        caf::anon_mail(r_id, -1).send(sender);
       }
     });
+  }
+
+  // Helper to map runtime index to compile-time index for tuple access
+  template <class F>
+  void dispatch_index(int idx, F&& f) {
+    dispatch_index_helper(std::make_index_sequence<sizeof...(Ts)>{}, idx, std::forward<F>(f));
+  }
+
+  template <std::size_t... Is, class F>
+  void dispatch_index_helper(std::index_sequence<Is...>, int idx, F&& f) {
+    (..., (static_cast<int>(Is) == idx ? f(std::integral_constant<std::size_t, Is>{}) : (void)0));
   }
 
   program_ptr  program_;
   nd_range     dims_;
   caf::actor_id actor_id_;
-  atom_value   reply_atom_;
+  int          reply_id_;
 };
 
 } // namespace caf::cuda
