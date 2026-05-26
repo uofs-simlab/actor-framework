fcadf40e74639a0188696f9e8ca4de7b05402df5
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2026-05-26 11:35:03 -0600

    fixed include paths

--- libcaf_cuda/caf/actorSOLVE/actorSOLVE.hpp
+++ libcaf_cuda/caf/actorSOLVE/actorSOLVE.hpp
@@ -1,4 +1,4 @@
 #pragma once
 #include "caf/actorBLAS/actorBLAS.hpp"
-#include "caf/actorSPARSE/actorSPASE.hpp"
+#include "caf/actorSPARSE/actorSPARSE.hpp"
 #include "caf/actorSOLVE/sparse-matrix-solvers/sparse-BiCGSTAB-actor/sparse-BiCGSTAB-actor.hpp"
