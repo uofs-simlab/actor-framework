b653ae31da6c276a75513089095ec96f32d36d1a
Dalton Beauchesne (nqr159) <nqr159@mail.usask.ca>
2026-05-28 14:32:26 -0600

    updated includes

--- libcaf_cuda/caf/actorSOLVE/actorSOLVE.hpp
+++ libcaf_cuda/caf/actorSOLVE/actorSOLVE.hpp
@@ -2,3 +2,6 @@
 #include "caf/actorBLAS/actorBLAS.hpp"
 #include "caf/actorSPARSE/actorSPARSE.hpp"
 #include "caf/actorSOLVE/sparse-matrix-solvers/sparse-BiCGSTAB-actor/sparse-BiCGSTAB-actor.hpp"
+#include "caf/actorSOLVE/sparse-matrix-solvers/sparse-GMRES-actor/sparse-GMRES-actor.hpp"
+#include "caf/actorSOLVE/sparse-matrix-solvers/sparse-CGS-actor/sparse-CGS-actor.hpp"
+
